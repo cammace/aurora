2ceee697ae4ae8450d0be26744dc2f11f38a7ac1
Cameron Mace <cameronmace10@gmail.com>
2018-09-07 11:34:51 -0400

    setup toolbar

--- app/src/main/java/com/cammace/aurora/MainActivity.kt
+++ app/src/main/java/com/cammace/aurora/MainActivity.kt
@@ -2,11 +2,38 @@ package com.cammace.aurora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.cammace.aurora.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    // Setup toolbar and hide title
+    setSupportActionBar(main_toolbar)
+    supportActionBar?.setDisplayShowTitleEnabled(false)
+    
+    binding.locationName = "New York"
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_toolbar, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    if (item?.itemId == R.id.action_search) {
+      Toast.makeText(this, "search clicked", Toast.LENGTH_LONG).show()
+    }
+
+    return super.onOptionsItemSelected(item)
   }
 }
